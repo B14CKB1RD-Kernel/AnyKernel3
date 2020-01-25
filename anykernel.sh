c7a07f0ad84fce46da1d44d589ae2b56c6605635
Sultan Alsawaf <sultan@kerneltoast.com>
2020-01-25 11:53:10 -0800

    Initial Sultan kernel support

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,14 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Caesium Kernel by msfjarvis
+kernel.string=Sultan Kernel for the Pixel 2 and Pixel 2 XL
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=walleye
-device.name2=taimen
-device.name3=wahoo
-device.name4=
-device.name5=
-supported.versions=9.0 - 10
-supported.patchlevels=2019-10 -
+device.name1=taimen
+device.name2=walleye
+supported.versions=10
 '; } # end properties
 
 # shell variables
@@ -23,44 +19,15 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-# set_perm_recursive 0 0 755 644 $ramdisk/*;
-# set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-# backup_file init.rc;
-# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-# backup_file init.tuna.rc;
-# insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-# append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-# backup_file fstab.tuna;
-# patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-# patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-# patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-# append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
-# If the kernel image and dtbs are separated in the zip
-decompressed_image=/tmp/anykernel/kernel/Image
-compressed_image=$decompressed_image.lz4
+decompressed_image=/tmp/anykernel/Image
+compressed_image=$decompressed_image.lz4-dtb
 if [ -f $compressed_image ]; then
   # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
   if [ -d $ramdisk/.backup ]; then
@@ -69,10 +36,6 @@ if [ -f $compressed_image ]; then
     $bin/magiskboot hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
     $bin/magiskboot compress=lz4 $decompressed_image $compressed_image;
   fi;
-
-  # Concatenate all of the dtbs to the kernel
-  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.lz4-dtb;
 fi;
 
 write_boot;
-## end install
